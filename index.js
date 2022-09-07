7c33c7395d462a66489d928b39222e5fd14bca0b
David <davidstepanov@yahoo.com>
2022-09-07 08:50:34 -0400

    commit tree efa916d7f7dd87a22179ed1e3923e4648e32ed3a

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+  return (res => res.json)
   // To pass the tests, don't forget to return your fetch!
-  
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
+    fetchBooks(book)
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
